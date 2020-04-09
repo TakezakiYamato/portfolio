0a6a37bb32195bd8925cc0259fa41159f5e0df00
TakezakiYamato <takezakigakuto@MacBook-Pro-3.local>
2020-04-09 17:28:59 +0900

    ポートフォリオ実装】20200409

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="mainSection">
-  つつ
-    <div id="userName">
-    つつ
-      <div id="companyName">
-        っっっf
-        <img src="" id="profilePicture">
-      </div>
-    </div>
+    <div id="userName">Yamato.takezaki</div>
+    <div id="companyName">seattle consulting</div>
+    <img src="@/assets/profilePicture.jpg" id="profilePicture"/>
   </div>
 </template>
+
 
 
 <script>
@@ -17,10 +13,28 @@
 </script>
 
 
-<style>
+<style scoped>
 #mainSection {
-  background: -moz-linear-gradient(top,#80E1F7,#06C1E6);
+  text-align: center;
+  background: linear-gradient(#80E1F7, #06C1E6);
   width: 100%;
   height: auto;
 }
+#userName {
+  color: #FFFFFF;
+  font-size: 18pt;
+  text-shadow: 1px 1px 3px #e8eaf6;
+}
+#companyName {
+  color: #FFFFFF;
+  font-size: 18pt;
+  font-type: bold;
+  text-shadow: 1px 1px 3px #e8eaf6;
+}
+#profilePicture {
+  border-radius: 50%;
+  width:64px;
+  height: 64px;
+}
+
 </style>
